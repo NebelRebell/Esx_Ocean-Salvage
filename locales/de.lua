31b15287ad4d8773398612d92d4b198a5b4bb269
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2018-08-28 15:57:13 +0200

    de.lua
    Included German translation

---
+++ locales/de.lua
@@ -0,0 +1,20 @@
+Locales['de'] = {
+	['GPS_info'] 			      = '~b~ Funkspruch: Ein neues Schiffswrack wurde im ~y~GPS markiert.',
+	['cancel_mission'] 		  = 'Du hast die ~s~Mission ~r~abgebrochen!',
+	['pickup'] 				      = 'Benutze ~INPUT_CONTEXT~ für das Schweissgerät.',
+	['end_service'] 		    = 'Service beenden',
+	['take_service'] 		    = 'Service annehmen',
+	['end_service_notif'] 	= '~r~Service beended!',
+	['take_service_notif'] 	= '~g~Service angenommen!',
+	['start_job'] 			    = 'Benutze ~b~F10 ~w~um den Job zu starten!',
+	['Vehicle_Menu_Title'] 	= 'Service Fahrzeug',
+	['in_vehicle'] 			    = '~r~Du darfst Dich hierzu nicht in einem Fahrzeug befinden!',
+	['vehicle_broken'] 		  = '~r~Du musst das Fahrzeug reparieren, bevor Du fortfahren kannst!',
+	['bad_vehicle'] 		    = 'Du kannst lediglich Dein ~b~Service Fahrzeug einparken!',
+	['not_good_veh']		    = '~r~Du musst Dich hierzu in Deinem ~b~Service Fahrzeug befinden!',
+	['stop_npc'] 			      = 'Job ~g~gescahfft~s~: Zurück zum Ausgangspunkt (Depot)!',
+}
+
+-- Translation by NebelRebell
+-- You need a translation?
+-- Write me ;) 've a nice day!
